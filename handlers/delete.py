95b2b143a739de540e92f3f10dba902aebaaf048
src80 <111764681+src80@users.noreply.github.com>
2022-08-24 02:09:31 +0300

    Update delete.py

--- handlers/delete.py
+++ handlers/delete.py
@@ -123,11 +123,9 @@ def delete(client, message,redis):
         Bot("restrictChatMember",{"chat_id": chatID,"user_id": userId,"can_send_messages": 0,"can_send_media_messages": 0,"can_send_other_messages": 0,
             "can_send_polls": 0,"can_change_info": 0,"can_add_web_page_previews": 0,"can_pin_messages": 0,"can_invite_users": 0,})
 
-if message.sticker:
-      Bot("deleteMessage",{"chat_id":chatID,"message_id":message.id})
-
+				
   if message.sticker:
-    if redis.sismember("{}Nbot:Lsticker".format(BOT_ID),chatID):#8
+    if rank == "asudo" and redis.get("{}Nbot:bodas".format(BOT_ID),chatID):
       Bot("deleteMessage",{"chat_id":chatID,"message_id":message.id})
       if redis.sismember("{}Nbot:Lsticker:res".format(BOT_ID),chatID):
         Bot("restrictChatMember",{"chat_id": chatID,"user_id": userId,"can_send_messages": 0,"can_send_media_messages": 0,"can_send_other_messages": 0,
